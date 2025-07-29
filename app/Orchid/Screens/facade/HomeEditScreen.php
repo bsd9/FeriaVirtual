8e18b1d83a34c954f10c45c0b21917b83dd5d8a6
Johan Garzon <johangarzonsalazar102@gmail.com>
2025-07-28 21:39:55 -0500

    Se suben cambios para pruebas

--- app/Orchid/Screens/facade/HomeEditScreen.php
+++ app/Orchid/Screens/facade/HomeEditScreen.php
@@ -5,42 +5,53 @@ namespace App\Orchid\Screens\facade;
 use App\Models\FacadeScreenFront;
 use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Group;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Fields\Select;
 use Orchid\Screen\Screen;
+use Orchid\Support\Facades\Alert;
 use Orchid\Support\Facades\Layout;
 use Orchid\Support\Facades\Toast;
 
 class HomeEditScreen extends Screen
 {
-
     public $principal;
 
+    /**
+     * Fetch data to be displayed on the screen.
+     *
+     * @return array
+     */
     public function query(FacadeScreenFront $principal): iterable
     {
-        if (! $principal->exists) {
-            $principal = new FacadeScreenFront();
+        if ($principal->exists) {
+            return [
+                'principal' => $principal,
+                'facade_screen_fronts' => [
+                    'nombre' => $principal->exists ? $principal->nombre : '',
+                    'publicidad1' => $principal->exists ? $principal->getFirstMediaUrl('publicidad1') : '',
+                ],
+            ];
         }
-
-        $this->principal = $principal;
-
+        
         return [
             'principal' => $principal,
-            'facade_screen_fronts' => [
-                'position' => $principal->position,
-                'publicidad1' => $principal->getFirstMediaUrl('publicidad1'),
-                'publicidad2' => $principal->getFirstMediaUrl('publicidad2'),
-                'publicidad3' => $principal->getFirstMediaUrl('publicidad3'),
-                'publicidad4' => $principal->getFirstMediaUrl('publicidad4'),
-            ],
         ];
     }
 
+    /**
+     * The name of the screen displayed in the header.
+     */
     public function name(): ?string
     {
-        return $this->principal->exists ? 'Editar Información' : 'Crear nueva publicidad';
+        return $this->principal->exists ? 'Editar Informacion' : 'Crear nueva publicidad';
     }
 
+    /**
+     * The screen's action buttons.
+     *
+     * @return \Orchid\Screen\Action[]
+     */
     public function commandBar(): iterable
     {
         return [
@@ -64,69 +75,128 @@ class HomeEditScreen extends Screen
         ];
     }
 
+    /**
+     * The screen's layout elements.
+     *
+     * @return \Orchid\Screen\Layout[]|string[]
+     */
     public function layout(): iterable
     {
         return [
-            // ✅ 1. Campos normales en Layout::rows
-            Layout::rows([
-                Input::make('principal.nombre')
-                    ->title('Nombre')
-                    ->placeholder('Ingresa el nombre de la feria')
-                    ->help('Especifica un título descriptivo corto para esta feria.')
-                    ->required()
-                    ->value($this->principal->nombre ?? ''),
+            Layout::columns([
 
-                Select::make('facade_screen_fronts.position')
-                ->options([
-                    'principal' => 'Principal',
-                    'right' => 'Segunda',
-                    'left' => 'Tercera',
-                ])
-                ->empty('Seleccione una posición.')
-                ->title('Posición')
-                ->help('Seleccione una posición')
-                ->value($this->principal->position ?? '')
+                Layout::rows([
+                    Input::make('principal.nombre')
+                        ->title('Nombre')
+                        ->placeholder('Ingresa el nombre de la feria')
+                        ->help('Especifica un título descriptivo corto para esta feria.')
+                        ->required()
+                        ->value($this->principal->exists ? $this->principal->nombre : ''),
+                    Group::make([
+                        Input::make('principal.publicidad1')
+                            ->title('Primera imagen')
+                            ->type('file')
+                            ->help('Sube una imagen para la publicidad #1')
+                            ->value($this->principal->exists ? $this->principal->getFirstMediaUrl('publicidad1') : ''),
+
+                        /*Input::make('facade_screen_fronts.publicidad2')
+                            ->title('Segunda imagen')
+                            ->type('file')
+                            ->help('Sube una imagen para la publicidad #2'),
+
+                        Input::make('facade_screen_fronts.publicidad3')
+                            ->title('Tercera imagen')
+                            ->type('file')
+                            ->help('Sube una imagen para la publicidad #3'),*/
+                    ]),
+                    /*Group::make([
+                        Input::make('facade_screen_fronts.publicidad4')
+                            ->title('Cuarta imagen')
+                            ->type('file')
+                            ->help('Sube una imagen para la publicidad #4'),
+                        Input::make('facade_screen_fronts.publicidad5')
+                            ->title('Quita imagen')
+                            ->type('file')
+                            ->help('Sube una imagen para la publicidad #5'),
+                    ]),
+                    Select::make('facade_screen_fronts.position')
+                        ->options([
+                            'principal' => 'Principal',
+                            'right' => 'derecha',
+                            'back' => 'Tracera',
+                            'left' => 'Izquierda',
+                        ])
+                        ->empty('Seleccione una.')
+                        ->title('Select tags')
+                        ->help('Seleccione una posicion'),
+
+                ]),
+                Layout::rows([
+                    Input::make('facade_screen_fronts.banner1')
+                        ->title('Banenr #1')
+                        ->placeholder('Ingresa el nombre de la feria')
+                        ->help('Imagen del banner numero 1')
+                        ->type('file')
+                        ->required(),
+
+                    Input::make('facade_screen_fronts.banner2')
+                        ->title('Banenr #2')
+                        ->placeholder('Ingresa el nombre de la feria')
+                        ->help('Imagen del banner numero 2')
+                        ->type('file')
+                        ->required(),
+                    Input::make('facade_screen_fronts.banner3')
+                        ->title('Banenr #3')
+                        ->placeholder('Ingresa el nombre de la feria')
+                        ->help('Imagen del banner numero 3')
+                        ->type('file')
+                        ->required(),
+                    Input::make('facade_screen_fronts.banner4')
+                        ->title('Banenr #4')
+                        ->placeholder('Ingresa el nombre de la feria')
+                        ->help('Imagen del banner numero 4')
+                        ->type('file')
+                        ->required(),*/
+                ]),
             ]),
 
-            // ✅ 2. Vista dinámica en su propio layout (nivel raíz)
-            Layout::view('layouts.dynamic-ads'),
         ];
     }
 
     public function createOrUpdate(Request $request, FacadeScreenFront $principal)
     {
-        $position = $request->input('facade_screen_fronts.position');
+        $request->validate([
+            'principal.nombre' => 'required',
+            'principal.publicidad1' => 'required',
+            /*'facade_screen_fronts.publicidad2' => 'required',
+            'facade_screen_fronts.publicidad3' => 'required',
+            'facade_screen_fronts.publicidad4' => 'required',
+            'facade_screen_fronts.publicidad5' => 'required',
+            'facade_screen_fronts.nombre' => 'required',
+            'facade_screen_fronts.banner1' => 'required',
+            'facade_screen_fronts.banner2' => 'required',
+            'facade_screen_fronts.banner3' => 'required',
+            'facade_screen_fronts.banner4' => 'required',*/
 
-        $rules = [
-            'principal.nombre' => 'required|string|max:255',
-            'principal.publicidad1' => 'required|file|image|max:2048',
-        ];
-
-        if ($position === 'right' || $position === 'left') {
-            $rules['facade_screen_fronts.publicidad2'] = 'required|file|image|max:2048';
-        }
-
-        if ($position === 'left') {
-            $rules['facade_screen_fronts.publicidad3'] = 'required|file|image|max:2048';
-            $rules['facade_screen_fronts.publicidad4'] = 'required|file|image|max:2048';
-        }
-
-        $request->validate($rules);
-
-        $principal->nombre = $request->input('principal.nombre');
-        $principal->position = $position;
-        $principal->save();
+        ]);
+        $this->clearAllMediaCollections($principal);
+        $this->addImagesToCollections($principal, $request);
+        $principal->fill([
+            'nombre' => $request->input('principal.nombre'),
+        ])->save();
 
         $this->clearAllMediaCollections($principal);
         $this->addImagesToCollections($principal, $request);
 
-        Toast::info('Cambios guardados exitosamente.');
+        Toast::info(__('Cambios realizados exitosamente.'));
+        Alert::info('Has creado o actualizado la feria correctamente.');
+
         return redirect()->route('platform.principals');
     }
 
     protected function clearAllMediaCollections(FacadeScreenFront $principal)
     {
-        $collections = ['publicidad1', 'publicidad2', 'publicidad3', 'publicidad4'];
+        $collections = ['publicidad1'/*, 'publicidad2', 'publicidad3', 'publicidad4', 'publicidad5', 'banner1', 'banner2', 'banner3', 'banner4'*/];
         foreach ($collections as $collection) {
             $principal->clearMediaCollection($collection);
         }
@@ -134,27 +204,50 @@ class HomeEditScreen extends Screen
 
     protected function addImagesToCollections(FacadeScreenFront $principal, Request $request)
     {
-        if ($request->hasFile('principal.publicidad1')) {
-            $principal->addMedia($request->file('principal.publicidad1'))->toMediaCollection('publicidad1');
+        $fields = [
+            'publicidad1'
+        ];
+        
+        foreach ($fields as $field) {
+            if ($request->hasFile("principal.{$field}")) {
+                $principal->addMedia($request->file("principal.{$field}"))
+                        ->toMediaCollection($field);
+            }
         }
-
+        /*if ($request->hasFile('facade_screen_fronts.publicidad1')) {
+            $principal->addMedia($request->file('facade_screen_fronts.publicidad1'))->toMediaCollection('publicidad1');
+        }
         if ($request->hasFile('facade_screen_fronts.publicidad2')) {
-            $principal->addMedia($request->file('facade_screen_fronts.publicidad2'))->toMediaCollection('publicidad2');
+            $principal->addMediaFromRequest('facade_screen_fronts.publicidad2')->toMediaCollection('publicidad2');
         }
-
         if ($request->hasFile('facade_screen_fronts.publicidad3')) {
-            $principal->addMedia($request->file('facade_screen_fronts.publicidad3'))->toMediaCollection('publicidad3');
+            $principal->addMediaFromRequest('facade_screen_fronts.publicidad3')->toMediaCollection('publicidad3');
         }
-
         if ($request->hasFile('facade_screen_fronts.publicidad4')) {
-            $principal->addMedia($request->file('facade_screen_fronts.publicidad4'))->toMediaCollection('publicidad4');
+            $principal->addMediaFromRequest('facade_screen_fronts.publicidad4')->toMediaCollection('publicidad4');
         }
+        if ($request->hasFile('facade_screen_fronts.publicidad5')) {
+            $principal->addMediaFromRequest('facade_screen_fronts.publicidad5')->toMediaCollection('publicidad5');
+        }
+        if ($request->hasFile('facade_screen_fronts.banner1')) {
+            $principal->addMediaFromRequest('facade_screen_fronts.banner1')->toMediaCollection('banner1');
+        }
+        if ($request->hasFile('facade_screen_fronts.banner2')) {
+            $principal->addMediaFromRequest('facade_screen_fronts.banner2')->toMediaCollection('banner2');
+        }
+        if ($request->hasFile('facade_screen_fronts.banner3')) {
+            $principal->addMediaFromRequest('facade_screen_fronts.banner3')->toMediaCollection('banner3');
+        }
+        if ($request->hasFile('facade_screen_fronts.banner4')) {
+            $principal->addMediaFromRequest('facade_screen_fronts.banner4')->toMediaCollection('banner4');
+        }*/
     }
 
     public function remove()
     {
         $this->principal->delete();
-        Toast::success('Feria eliminada correctamente.');
-        return redirect()->route('platform.principals');
+        Alert::success('Éxito. Has eliminado la compañía con éxito.');
+
+        return redirect()->route('platform.pricipals');
     }
 }
